fac7673e3cdd64ba7c4b9fe5344f4ad798faee27
AndreaWesterinen <arwesterinen@gmail.com>
2021-04-02 15:06:16 -0400

    Corrected naming

--- tools/create_merged_ontol_and_tree.sh
+++ tools/create_merged_ontol_and_tree.sh
@@ -3,6 +3,6 @@
 
 cd ../ontologies
 java -jar ../tools/robot.jar merge --inputs "*.ttl" --output ../dna-ontology.ttl
-java -jar ../tools/robot.jar annotate --input ../dna-ontology.ttl --ontology-iri "urn:logx:ontology" --output ../annotated-ontology.ttl
+java -jar ../tools/robot.jar annotate --input ../dna-ontology.ttl --ontology-iri "urn:ontoinsights:ontology:dna" --output ../annotated-ontology.ttl
 java -jar ../tools/robot.jar tree --input ../annotated-ontology.ttl --tree ../ontol-docs/dna-ontology-tree.html
 rm ../annotated-ontology.ttl
